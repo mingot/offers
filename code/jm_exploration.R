0f4b45c0f9219d68f8aff04e558de6877a107e72
Josep Marc Mingot Hidalgo <jm.mingot@gmail.com>
2014-05-20 09:28:25 +0200

    added features times per month, frequency and product popularity

--- code/jm_exploration.R
+++ code/jm_exploration.R
@@ -1,5 +1,43 @@
 
+# Sample transactions for test
+transactions = read.table(file="data/sample/sample.txt", header=T, sep=",")
+ids = unique(transactions$id)
+trainSample = train[train$id %in% ids,]
+
+# Conditional probabilities to success
+train$repeater = 0
+train$repeater[train$repeattrips>0] = 1
+t = aggregate(train$repeater, by=list(train$offer), FUN=sum)
+t2 = aggregate(train$repeater, by=list(train$offer), FUN=length)
+t = merge(t,t2, by="Group.1", all.x=T)
+t$prob = t$x.x/t$x.yn
+names(t)[1] = "offer"
+names(t)[4] = "prob"
+t = t[,c("offer","prob")]
+t = t[order(-t$prob),]
+row.names(t) = NULL
+
+
 t = data.frame(TRAIN)
 summary(t[,11:16])
 # BRAND
 head(S)
+
+#offers model
+aux = setNames(aggregate(train$repeater, by=list(train$offer), FUN=mean), c("offer", "prior"))
+offersTrain = merge(offers, aux, all.y=T)
+offersTest = offers[!(offers$offer %in% offersTrain$offer), ]
+
+
+sum(offersTest$category %in% offersTrain$category)
+
+offers.glm = lm(prior~offervalue, offersTrain)
+
+
+
+
+
+
+
+
+
--- code/jm_main.R
+++ code/jm_main.R
@@ -4,93 +4,95 @@ train = read.table(file="data/original/trainHistory.csv", header=T, sep=",")
 test = read.table(file="data/original/testHistory.csv", header=T, sep=",")
 offers = read.table(file="data/original/offers.csv", header=T, sep=",")
 
-# Sample transactions for test
-transactions = read.table(file="data/sample/sample.txt", header=T, sep=",")
-ids = unique(transactions$id)
-trainSample = train[train$id %in% ids,]
-
-# Conditional probabilities to success
-train$repeater = 0
-train$repeater[train$repeattrips>0] = 1
-t = aggregate(train$repeater, by=list(train$offer), FUN=sum)
-t2 = aggregate(train$repeater, by=list(train$offer), FUN=length)
-t = merge(t,t2, by="Group.1", all.x=T)
-t$prob = t$x.x/t$x.yn
-names(t)[1] = "offer"
-names(t)[4] = "prob"
-t = t[,c("offer","prob")]
-t = t[order(-t$prob),]
-row.names(t) = NULL
-
-
-t2 = data.frame(TEST)
-t2[is.na(t2)] = 0
-
-t[sample(1:nrow(t),10),6:16]
-t2[sample(1:nrow(t),10),6:14]
-
-
 
 # Extract Features --------------------------------------------------------
-
 library(data.table)
 library(bit64)
 TRAIN = data.table(merge(train, offers[,c("offer","company","brand","category")], all.x=T))
 TRANS = fread("data/sample/transactions_train.csv", header=T)
+TEST = data.table(merge(test, offers[,c("offer","company","brand","category")], all.x=T))
+TRANS_TEST = fread("data/sample/transactions_test.csv")
+
 setnames(TRANS, c("id","chain","dept","category","company","brand","date","productsize","productmeasure","purchasequantity","purchaseamount"))
 
-# convert from int64 to numeric some fields
-set(TRANS, j="id", value=as.numeric(TRANS[["id"]]))
-set(TRANS, j="company", value=as.numeric(TRANS[["company"]]))
-
-# brand grouping
-setkey(TRANS,id,brand)
-setkey(TRAIN,id,brand)
-S = TRANS[,list(BRANDquant=sum(purchasequantity), BRANDamount=sum(purchaseamount)), by=list(id,brand)]
-TRAIN = merge(TRAIN, S, all.x=T)
-
-# company grouping
-setkey(TRANS,id,company)
-setkey(TRAIN,id,company)
-S = TRANS[,list(COMPquant=sum(purchasequantity), COMPamount=sum(purchaseamount)), by=list(id,company)]
-TRAIN = merge(TRAIN, S, all.x=T)
-
-# category grouping
-setkey(TRANS,id,category)
-setkey(TRAIN,id,category)
-S = TRANS[,list(CATquant=sum(purchasequantity), CATamount=sum(purchaseamount)), by=list(id,category)]
-TRAIN = merge(TRAIN, S, all.x=T) 
-
-# average ticket
-S = TRANS[,list(aov=sum(purchaseamount)), by=list(id, date)]
-S = S[,list(aov=mean(aov)), by=id]
-TRAIN = merge(TRAIN, S,by=c("id"), all.x=T)
-
-# buy times per month
-S = TRANS[,list(aov=length(purchaseamount)), by=list(id, date)]
-S = S[,month:=month(as.Date(S[["date"]]))]
-S = S[,year:=year(as.Date(S[["date"]]))]
-S = S[,list(times=length(id)),by=list(id,month,year)]
-
-# data treatment
-t = data.frame(TRAIN)
-t$repeater = 0
-t$repeater[t$repeattrips>0] = 1
-
-# NA treatment
-t[is.na(t)] = 0
-
-# check for brands
-t$check_brand = 0
-t[t$BRANDquant!=0,"check_brand"] = 1
-t$check_category = 0
-t[t$CATquant!=0,"check_category"] = 1
-t$check_company = 0
-t[t$COMPquant!=0,"check_company"] = 1
+# Offers priors
+train$repeater = 0
+train$repeater[train$repeattrips>0] = 1
+offPrior = aggregate(train$repeater, by=list(train$offer), FUN=mean)
+names(offPrior) = c("offer","offer_prior")
+offPrior = offPrior[offPrior$offer %in% unique(test$offer),]
 
 
-t[,11:16] = scale(t[,11:16])
-t$aov_sc = scale(t$aov)
+ExtractFeatures = function(TRANS, TRAIN){
+  # convert from int64 to numeric some fields
+  set(TRANS, j="id", value=as.numeric(TRANS[["id"]]))
+  set(TRANS, j="company", value=as.numeric(TRANS[["company"]]))
+  
+  # brand grouping
+  setkey(TRANS,id,brand)
+  setkey(TRAIN,id,brand)
+  S = TRANS[,list(BRANDquant=sum(purchasequantity), BRANDamount=sum(purchaseamount)), by=list(id,brand)]
+  TRAIN = merge(TRAIN, S, all.x=T)
+  
+  # company grouping
+  setkey(TRANS,id,company)
+  setkey(TRAIN,id,company)
+  S = TRANS[,list(COMPquant=sum(purchasequantity), COMPamount=sum(purchaseamount)), by=list(id,company)]
+  TRAIN = merge(TRAIN, S, all.x=T)
+  
+  # category grouping
+  setkey(TRANS,id,category)
+  setkey(TRAIN,id,category)
+  S = TRANS[,list(CATquant=sum(purchasequantity), CATamount=sum(purchaseamount)), by=list(id,category)]
+  TRAIN = merge(TRAIN, S, all.x=T) 
+  
+  # AOV
+  S = TRANS[,list(aov=sum(purchaseamount)), by=list(id, date)]
+  S = S[,list(aov=mean(aov)), by=id]
+  TRAIN = merge(TRAIN, S, by="id", all.x=T)
+  
+  # frequency (times per month)
+  S = TRANS[,list(aov=length(purchaseamount)), by=list(id, date)] #group by dayly transactions
+  S = S[, month:=substr(S[["date"]],1,7)] # extract month-year
+  S = S[, times:=.N, by=list(id,month)] # group by month
+  S = S[, list(freq=mean(times)), by=id] # mean per user
+  TRAIN = merge(TRAIN, S, by="id", all.x=T)
+  
+  # number of times the offered product was bought
+  S = TRANS[,list(product_times=.N), by=list(category,brand,company)]
+  TRAIN = merge(TRAIN, S, by=c("brand","company","category"), all.x=T)
+  
+  # number of different users that boutht the product
+  
+  # data treatment
+  t = data.frame(TRAIN)
+  
+  # check for brands
+  t[is.na(t)] = 0 # NA treatment
+  t$check_brand = 0
+  t[t$BRANDquant!=0,"check_brand"] = 1
+  t$check_category = 0
+  t[t$CATquant!=0,"check_category"] = 1
+  t$check_company = 0
+  t[t$COMPquant!=0,"check_company"] = 1
+  
+  # Scaling
+  # t[,11:16] = scale(t[,11:16])
+  t$aov_sc = scale(t$aov)
+  t$freq_sc = scale(t$freq)
+  t$product_times_sc = scale(t$product_times)
+  
+  # priors to known offers
+  t = merge(t, offPrior, all.x=T) 
+  
+  # NA treatment
+  t[is.na(t)] = 0
+
+  return(t)
+}
+
+t = ExtractFeatures(TRANS, TRAIN)
+tTest = ExtractFeatures(TRANS_TEST, TEST)
 
 # training ----------------------------------------------------------------
 library(pROC)
@@ -107,7 +109,8 @@ for (i in 1:k){
   # Training
 #   fit.glm = glm(factor(repeater) ~ BRANDquant + BRANDamount + CATquant + CATamount + COMPquant + COMPamount, data=trainingset, family=binomial)
 #   fit.glm = glm(repeater ~ BRANDamount + CATquant + COMPquant + COMPamount, data=trainingset, family=binomial)
-  fit.glm = glm(repeater ~ check_company + check_category + check_brand + aov_sc, data=trainingset, family=binomial)
+  fit.glm = glm(repeater ~ check_company + check_category + check_brand + aov_sc + freq_sc + product_times_sc, 
+                data=trainingset, family=binomial)
   
   # Testing
   pred = predict(fit.glm, testset, type="response")
@@ -128,49 +131,49 @@ summary(scale(t[,c("BRANDamount","CATquant","COMPquant","COMPamount")]))
 
 # test import -------------------------------------------------------------
 
-TEST = data.table(merge(test, offers[,c("offer","company","brand","category")], all.x=T))
-TRANS = fread("data/sample/transactions_test.csv")
-setnames(TRANS, c("id","chain","dept","category","company","brand","date","productsize","productmeasure","purchasequantity","purchaseamount"))
+# TEST = data.table(merge(test, offers[,c("offer","company","brand","category")], all.x=T))
+# TRANS = fread("data/sample/transactions_test.csv")
+# setnames(TRANS, c("id","chain","dept","category","company","brand","date","productsize","productmeasure","purchasequantity","purchaseamount"))
+# 
+# # convert to numeric some fields
+# set(TRANS, j="id", value=as.numeric(TRANS[["id"]]))
+# set(TRANS, j="company", value=as.numeric(TRANS[["company"]]))
+# 
+# # brand grouping
+# setkey(TRANS,id,brand)
+# setkey(TEST,id,brand)
+# S = TRANS[,list(BRANDquant=sum(purchasequantity), BRANDamount=sum(purchaseamount)), by=list(id,brand)]
+# TEST = merge(TEST, S, all.x=T)
+# 
+# # company grouping
+# setkey(TRANS,id,company)
+# setkey(TEST,id,company)
+# S = TRANS[,list(COMPquant=sum(purchasequantity), COMPamount=sum(purchaseamount)), by=list(id,company)]
+# TEST = merge(TEST, S, all.x=T)
+# 
+# # category grouping
+# setkey(TRANS,id,category)
+# setkey(TEST,id,category)
+# S = TRANS[,list(CATquant=sum(purchasequantity), CATamount=sum(purchaseamount)), by=list(id,category)]
+# TEST = merge(TEST, S, all.x=T)  
+# 
+# # average ticket
+# S = TRANS[,list(aov=sum(purchaseamount)), by=list(id, date)]
+# S = S[,list(aov=mean(aov)), by=id]
+# TEST = merge(TEST, S,by=c("id"), all.x=T)
+# 
+# # Apply the ML algorithm
+# t = data.frame(TEST)
+# t[is.na(t)] = 0
+# t$check_brand = 0
+# t[t$BRANDquant!=0,"check_brand"] = 1
+# t$check_category = 0
+# t[t$CATquant!=0,"check_category"] = 1
+# t$check_company = 0
+# t[t$COMPquant!=0,"check_company"] = 1
+# t$aov_sc = scale(t$aov)
 
-# convert to numeric some fields
-set(TRANS, j="id", value=as.numeric(TRANS[["id"]]))
-set(TRANS, j="company", value=as.numeric(TRANS[["company"]]))
-
-# brand grouping
-setkey(TRANS,id,brand)
-setkey(TEST,id,brand)
-S = TRANS[,list(BRANDquant=sum(purchasequantity), BRANDamount=sum(purchaseamount)), by=list(id,brand)]
-TEST = merge(TEST, S, all.x=T)
-
-# company grouping
-setkey(TRANS,id,company)
-setkey(TEST,id,company)
-S = TRANS[,list(COMPquant=sum(purchasequantity), COMPamount=sum(purchaseamount)), by=list(id,company)]
-TEST = merge(TEST, S, all.x=T)
-
-# category grouping
-setkey(TRANS,id,category)
-setkey(TEST,id,category)
-S = TRANS[,list(CATquant=sum(purchasequantity), CATamount=sum(purchaseamount)), by=list(id,category)]
-TEST = merge(TEST, S, all.x=T)  
-
-# average ticket
-S = TRANS[,list(aov=sum(purchaseamount)), by=list(id, date)]
-S = S[,list(aov=mean(aov)), by=id]
-TEST = merge(TEST, S,by=c("id"), all.x=T)
-
-# Apply the ML algorithm
-t = data.frame(TEST)
-t[is.na(t)] = 0
-t$check_brand = 0
-t[t$BRANDquant!=0,"check_brand"] = 1
-t$check_category = 0
-t[t$CATquant!=0,"check_category"] = 1
-t$check_company = 0
-t[t$COMPquant!=0,"check_company"] = 1
-t$aov_sc = scale(t$aov)
-
-pred = predict(fit.glm, t, type="response")
+pred = predict(fit.glm, tTest, type="response")
 pred[pred>1]=1
 
 
@@ -180,5 +183,5 @@ d = as.character(Sys.time())
 d = gsub(":","_", d)
 d = gsub("-","_", d)
 d = gsub(" ","_", d)
-write.table(data.frame(id=t$id, repeatProbability=pred), file=paste("data/submissions/sub",d,".csv",sep=""),row.names=F, quote=F, sep=",")
+write.table(data.frame(id=tTest$id, repeatProbability=pred), file=paste("data/submissions/sub",d,".csv",sep=""),row.names=F, quote=F, sep=",")
 
